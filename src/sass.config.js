b7f3dd267802ba9111cef8ab44dc54c88cb6fe61
18231550+reignreapsow@users.noreply.github.com <18231550+reignreapsow@users.noreply.github.com>
2020-11-21 09:43:42 -0500

    DART SASS CONFIG ERROR

---
+++ src/sass.config.js
@@ -0,0 +1,13 @@
+var sass = require('sass');
+
+sass.render({
+  file: scss_filename
+}, function(err, result) {
+  /* ... */
+});
+
+// OR
+
+var result = sass.renderSync({
+  file: scss_filename
+});
